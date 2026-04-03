f8d8ac30b97ef2e9867f31852aef772eaf733c04
Anctw <3180458409@qq.com>
2026-04-03 22:05:40 +0800

    11.7——Fix the bug

--- 11.Javascript逆向爬虫/11.7浏览器环境下JavaScript的模拟执行/js_test.py
+++ 11.Javascript逆向爬虫/11.7浏览器环境下JavaScript的模拟执行/js_test.py
@@ -12,7 +12,7 @@ browser = context.chromium.launch()
 page = browser.new_page()
 
 page.route(
-    "/js/chunk-10192a00.243cb8b7.js",
+    "**/js/chunk-10192a00.243cb8b7.js",
     lambda route: route.fulfill(path="./chunk.js")
 )
 
